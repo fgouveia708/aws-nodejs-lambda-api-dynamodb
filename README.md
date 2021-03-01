ef8b2eed7854514b728a5c4052c64ff47ecaa422
Fernando Gouveia <fernandogouveia@outlook.com>
2021-03-01 12:23:55 -0300

    initial code

---
+++ README.md
@@ -0,0 +1,140 @@
+
+
+# CRUD API para AWS Lambda com DynamoDB (NodeJS)
+
+CRUD API para AWS Lambda com DynamoDB (NodeJS) é uma aplicação que demonstra a utilização de AWS Lambda, API Gateway e DynamaDB para criação de APIs.
+
+- [Arquitetura](https://github.com/fgouveia708/aws-nodejs-lambda-api-dynamodb#arquitetura)
+    - [Caso de uso](https://github.com/fgouveia708/aws-nodejs-lambda-api-dynamodb#caso-de-uso)
+    - [Setup](https://github.com/fgouveia708/aws-nodejs-lambda-api-dynamodb#setup)
+    - [Notas musicais](https://github.com/fgouveia708/aws-nodejs-lambda-api-dynamodb#deployment)
+    - [Uso](https://github.com/fgouveia708/aws-nodejs-lambda-api-dynamodb#uso)
+
+### Arquitetura
+
+
+
+## Caso de uso
+
+- API para Web Application
+- API para Mobile Application
+
+## Setup
+
+1. Instalar [NodeJS](https://nodejs.org/en/)
+2. Instalar [Serverless](https://www.serverless.com/)
+
+```bash
+npm install serverless -g
+```
+
+3. Instalar todas dependências do projeto
+   
+```bash
+npm install
+```
+
+## Deployment
+
+1. Configurar AWS credentials:
+   
+```bash
+export AWS_ACCESS_KEY_ID="..."
+export AWS_SECRET_ACCESS_KEY="..."
+export AWS_SESSION_TOKEN="..."
+```
+
+1. Publicar aplicação:
+
+```bash
+serverless deploy
+```
+
+O resultado esperado deve ser semelhante a:
+
+```bash
+Serverless: Packaging service...
+Serverless: Excluding development dependencies...
+
+Service Information
+service: serverless-lambda-api-with-dynamodb
+stage: dev
+region: us-east-1
+stack: serverless-lambda-api-with-dynamodb-dev
+resources: 37
+api keys:
+  None
+endpoints:
+  POST - https://xxxxxxxxxx.execute-api.us-east-1.amazonaws.com/dev/contact
+  GET - https://xxxxxxxxxx.execute-api.us-east-1.amazonaws.com/dev/contacts
+  GET - https://xxxxxxxxxx.execute-api.us-east-1.amazonaws.com/dev/contact/{id}
+  PUT - https://xxxxxxxxxx.execute-api.us-east-1.amazonaws.com/dev/contact/{id}
+  DELETE - https://xxxxxxxxxx.execute-api.us-east-1.amazonaws.com/dev/contact/{id}
+functions:
+  create: serverless-lambda-api-with-dynamodb-dev-create
+  getAll: serverless-lambda-api-with-dynamodb-dev-getAll
+  get: serverless-lambda-api-with-dynamodb-dev-get
+  update: serverless-lambda-api-with-dynamodb-dev-update
+  delete: serverless-lambda-api-with-dynamodb-dev-delete
+layers:
+  None
+```
+
+## Uso
+
+Você pode criar, recuperar, atualizar ou excluir contatos com os seguintes comandos:
+
+### Criar um contato
+
+```bash
+curl -X POST https://XXXXXXX.execute-api.us-east-1.amazonaws.com/dev/contact --data '{"name":"Robert","email":"email@email.com","phone":"01234567890"}'
+```
+
+Exemplo do resultado:
+```bash
+{"id":"379c41f0-787b-11eb-8759-37b6eff35e6e","name":"Robert","email":"email@email.com","phone":"01234567890","createdAt":1614375617422,"updatedAt":1614375617422}
+```
+
+### Buscar todos os contatos
+
+```bash
+curl https://XXXXXXX.execute-api.us-east-1.amazonaws.com/dev/contacts
+```
+
+Exemplo do resultado:
+```bash
+[{"id":"379c41f0-787b-11eb-8759-37b6eff35e6e","name":"Robert","email":"email@email.com","phone":"01234567890","createdAt":1614375617422,"updatedAt":1614375617422},{"id":"379c41f0-787b-11eb-8759-37b6eff35e6e","name":"Mary","email":"email@email.com","phone":"01234567890","createdAt":1614375617422,"updatedAt":1614375617422}]
+```
+
+### Buscar um contato
+
+```bash
+# Substitua a parte <id> por uma id real da sua tabela de contatos
+curl https://XXXXXXX.execute-api.us-east-1.amazonaws.com/dev/contact/<id>
+```
+
+Exemplo do resultado:
+```bash
+{"id":"379c41f0-787b-11eb-8759-37b6eff35e6e","name":"Robert","email":"email@email.com","phone":"01234567890","createdAt":1614375617422,"updatedAt":1614375617422}
+```
+
+### Atualizar um contato
+
+```bash
+# Substitua a parte <id> por uma id real da sua tabela de contatos
+curl -X PUT https://XXXXXXX.execute-api.us-east-1.amazonaws.com/dev/contact/<id> --data '{"id":"379c41f0-787b-11eb-8759-37b6eff35e6e","name":"Robert","email":"email@email.com","phone":"01234567890","createdAt":1614375617422,"updatedAt":1614375617422}'
+```
+
+Exemplo do resultado:
+```bash
+{"id":"379c41f0-787b-11eb-8759-37b6eff35e6e","name":"Robert","email":"email@email.com","phone":"01234567890","createdAt":1614375617422,"updatedAt":2314375617422}
+```
+
+### Excluir um contato
+
+```bash
+# Substitua a parte <id> por uma id real da sua tabela de contatos
+curl -X DELETE https://XXXXXXX.execute-api.us-east-1.amazonaws.com/dev/contact/<id>
+```
+
+Sem resultado
---
+++ handler.js
@@ -0,0 +1,203 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+// Create a contact
+module.exports.create = (event, context, callback) => {
+
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+
+  if (typeof data.name !== 'string' || typeof data.email !== 'string' || typeof data.phone !== 'string') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create the contact.',
+    });
+    return;
+  }
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: uuid.v1(),
+      name: data.name,
+      email: data.email,
+      phone: data.phone,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
+  };
+
+  // write the contact to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the contact.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+};
+
+// Get all contacts
+module.exports.getAll = (event, context, callback) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+  };
+
+   // fetch all contacts from the database
+   dynamoDb.scan(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the contacts.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    };
+    callback(null, response);
+  });
+};
+
+// Get a contact
+module.exports.get = (event, context, callback) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+  // fetch contact from the database
+  dynamoDb.get(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the contact.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+    callback(null, response);
+  });
+};
+
+// Update a contact
+module.exports.update = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+
+  // validation
+  if (typeof data.name !== 'string' || typeof data.email !== 'string' || typeof data.phone !== 'string') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t update the contact.',
+    });
+    return;
+  }
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+    ExpressionAttributeNames: {
+      '#contact_name': 'name',
+    },
+    ExpressionAttributeValues: {
+      ':name': data.name,
+      ':email': data.email,
+      ':phone': data.phone,
+      ':updatedAt': timestamp,
+      ':createdAt':data.createdAt
+    },
+    UpdateExpression: 'SET #contact_name = :name, email = :email, phone = :phone, createdAt = :createdAt, updatedAt = :updatedAt',
+    ReturnValues: 'ALL_NEW',
+  };
+
+
+  // update the contact in the database
+  dynamoDb.update(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the contact.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Attributes),
+    };
+    callback(null, response);
+  });
+};
+
+// Delete a contact
+module.exports.delete = (event, context, callback) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+  // delete the contact from the database
+  dynamoDb.delete(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t remove the contact.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({}),
+    };
+    callback(null, response);
+  });
+};
